72fd9182e02c1811dc1d7e7e51a274aeb57009b5
Hesam Sameni <hesam.sameni@gmail.com>
2016-09-27 02:24:11 +0330

    7th

--- app/src/main/java/org/vaya_group/telegram_stickers/channelsF.java
+++ app/src/main/java/org/vaya_group/telegram_stickers/channelsF.java
@@ -1,7 +1,112 @@
 package org.vaya_group.telegram_stickers;
 
-/**
- * Created by Sameni on 25/09/2016.
- */
-public class channelsF {
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class channelsF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.channelsf);
+    }
+
+
+    public void boaringclass (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/boringclass"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+    public void likeyoubro (View view){
+
+    Uri uri = Uri.parse("https://telegram.me/updateee"); // missing 'http://' will cause crashed
+    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    startActivity(intent);
+}
+
+
+
+    public void stickerchannel (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/stickerschannel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void gifchannel (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/gifschannel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void musicalbum (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/fullalbums"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void programmerjokes (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/programmerjokes"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void commonmistakes (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/common_mistakes"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void crynet (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/crynet"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void tvshare (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/thetvshare"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void sickmind (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/SickMindsMedia"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void moviehd (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/moviehdchannel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void sickosadism (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/Sickosadism"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void goback (View view){
+        Intent i = new Intent(getApplicationContext(), channels.class);
+        startActivity(i);
+        overridePendingTransition(R.animator.activity_push_up_in, R.animator.activity_push_up_out);
+    }
 }
--- app/src/main/java/org/vaya_group/telegram_stickers/channelsI.java
+++ app/src/main/java/org/vaya_group/telegram_stickers/channelsI.java
@@ -1,7 +1,206 @@
 package org.vaya_group.telegram_stickers;
 
-/**
- * Created by Sameni on 25/09/2016.
- */
-public class channelsI {
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class channelsI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.channelsi);
+    }
+
+
+    public void varzesh3 (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/varzesh3_channel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+    public void khabarefori (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/akhbarefori"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+
+    public void khoroosjangi (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/Khooroosjangi"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void bedandid (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/bedandid"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void gizmiz (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/gizmiztel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void jokefarsi (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/jokefarsiii"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void alvandmusic (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/alvandmusic"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void yjc (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/yjcnewschannel"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void pandaneh (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/pandaane"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void axojoke (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/aksojok"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void ajibvalivaghei (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/Ajibvalivaghaei"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void dirindirin (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/dirindirin"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void coffeegram (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/coffeegram"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void drsalamat (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/DrSalamatXXX"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void lezateashpazi (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/Lezat_ashpazi"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void farsna (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/farsna"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void channel90 (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/TVnavad"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void eslahatnews (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/eslahatnews"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void donyayeashpazi (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/tchef"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    public void isna (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/isna94"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void golagha (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/GOLAGHA"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void rajanews (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/rajanews_com"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void khandevaneh (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/kandevane"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void digiato (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/digiato_official"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void varzesh (View view){
+
+        Uri uri = Uri.parse("https://telegram.me/VarzeshtvIRIB"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+
+
+
+    public void goback (View view){
+        Intent i = new Intent(getApplicationContext(), channels.class);
+        startActivity(i);
+        overridePendingTransition(R.animator.activity_push_up_in, R.animator.activity_push_up_out);
+    }
 }
